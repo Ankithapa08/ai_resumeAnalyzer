6736448c8fcf430798b08d55884eb82aa7df5202
Ankit Thapa <lt17086@gmail.com>
2026-05-18 22:35:18 +0530

    cors link

--- backend-node/server.js
+++ backend-node/server.js
@@ -5,7 +5,9 @@ const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const app = express();
 connectDB();
-app.use(cors());
+app.use(cors({
+    origin :"https://ai-resume-analyzer-seven-ochre.vercel.app/"
+}));
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 const uploadRoutes = require("./routes/uploadRoutes");
