e86dc7bae41dab05e581c36c833a8ad297a2f714
Ankit Thapa <lt17086@gmail.com>
2026-05-19 03:32:03 +0530

    v1

--- backend-node/routes/uploadRoutes.js
+++ backend-node/routes/uploadRoutes.js
@@ -4,6 +4,9 @@ const authMiddleware = require("../middleware/authMiddleware");
 const fs = require("fs");
 
 const router = express.Router();
+if (!fs.existsSync("uploads")) {
+   fs.mkdirSync("uploads");
+}
 const storage = multer.diskStorage({
     destination :(req,file,cb) => {
         cb(null,"uploads/");
